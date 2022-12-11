31af768390ec2d489bf2111c287150cd2756997e
igorminash <iminash@inbox.ru>
2022-12-11 11:01:14 +0300

    Добавлено решение задачи 64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N,1));
+
+
+string PrintNumbers(int start, int end){
+    if(start == end) return start.ToString();
+    return(start + " " + PrintNumbers(start -1, end));
+}
