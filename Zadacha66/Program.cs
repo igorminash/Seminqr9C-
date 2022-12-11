2b2eaae8df1e12510de40ca6fd83989fe58c3ae3
igorminash <iminash@inbox.ru>
2022-12-11 11:03:30 +0300

    Добавлено решение задачи 66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(M,N));
+
+
+int PrintNumbers(int start, int end){
+    if(start == end) return start;
+    return(start + PrintNumbers(start + 1, end));
+}
