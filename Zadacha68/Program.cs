5cbfca44a6aa8fee1ddfaef085032e474e28f599
igorminash <iminash@inbox.ru>
2022-12-11 11:42:45 +0300

    Добавлено решение задачи 68

--- Zadacha68/Program.cs
+++ Zadacha68/Program.cs
@@ -6,11 +6,11 @@ int n = int.Parse(Console.ReadLine()!);
 Console.WriteLine($"{GetAkkerman (m,n)}");
 
 int GetAkkerman (int m, int n){
-    if (m == 0) 
-        return n + 1;
-    if (m != 0 && n == 0) 
-        return GetAkkerman(m-1,1);
-    if (m > 0 && n > 0) 
-        return GetAkkerman(m-1,GetAkkerman(m, n-1));
-return GetAkkerman(m,n);
+    if (m == 0) return n + 1;
+    else{
+        if (n == 0) return GetAkkerman(m-1,1);
+        else{
+            return GetAkkerman(m-1,GetAkkerman(m, n-1));
+        }
+    }
 }
